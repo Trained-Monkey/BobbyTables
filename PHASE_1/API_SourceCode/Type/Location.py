a8d0682841ba1791568fd39bc17df908cffa2246
Michael Chen <chaosspaces11@gmail.com>
2022-03-15 17:29:46 +1100

    Updated git ignore, added examples

--- PHASE_1/API_SourceCode/Type/Location.py
+++ PHASE_1/API_SourceCode/Type/Location.py
@@ -3,3 +3,11 @@ from pydantic import BaseModel
 class Location(BaseModel):
     country: str
     location: str
+
+    class Config:
+        schema_extra = {
+            'example': {
+               "country": "China",
+               "location": "Wuhan, Hubei Province"
+            }
+        }
