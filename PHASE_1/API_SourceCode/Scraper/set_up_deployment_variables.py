38ec6c32fa93cdbfc02dd64c1401d347111173b3
Cooper Bartlett <cooperbar@gmail.com>
2022-03-17 21:52:36 +1100

    Creates scripts to autopopulate .env file from github

---
+++ PHASE_1/API_SourceCode/Scraper/set_up_deployment_variables.py
@@ -0,0 +1,29 @@
+import os
+import yaml
+
+# Set up .env
+with open('.env', 'w') as f:
+    lines = ['MONGODB_USER=' + os.environ.get('MONGODB_USER'), 'MONBODB_PASSWORD=' + os.environ.get('MONBODB_PASSWORD'),
+             'GC_SERVICE_ACC_PRIVATE_KEY_ID=' + os.environ.get('GC_SERVICE_ACC_PRIVATE_KEY_ID'),
+             'GC_SERVICE_ACC_PRIVATE_KEY=' + os.environ.get('GC_SERVICE_ACC_PRIVATE_KEY'),
+             'GC_SERVICE_ACC_CLIENT_EMAIL=' + os.environ.get('GC_SERVICE_ACC_CLIENT_EMAIL'),
+             'GC_SERVICE_ACC_CLIENT_ID=' + os.environ.get('GC_SERVICE_ACC_CLIENT_ID')]
+    f.writelines(lines)
+
+
+# Reformat requirements.txt
+with open('requirements.txt', 'r') as f:
+    requirements = f.readlines()
+# Remove scrapy from requirements
+requirements = requirements[1:]
+with open('requirements.txt', 'w') as f:
+    f.writelines(requirements)
+
+# Set up scrapinghub.yml
+with open('scrapinghub.yml', 'r') as f:
+    d = yaml.load(f.read(), Loader=yaml.FullLoader)
+    d['apikey'] = os.environ['APIKEY']
+
+# update config to file
+with open('scrapinghub.yml', 'w') as f:
+    yaml.dump(d, f, default_flow_style=False)
