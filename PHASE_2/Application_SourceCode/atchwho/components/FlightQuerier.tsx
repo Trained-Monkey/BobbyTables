7547fe558d5f6903a390d15fed516dc483b83923
Ben Zhou <you@example.com>
2022-04-19 11:54:36 +0800

    flight recommendation

---
+++ PHASE_2/Application_SourceCode/atchwho/components/FlightQuerier.tsx
@@ -0,0 +1,68 @@
+import * as React from 'react';
+import { useState, forwardRef } from 'react';
+import { ListGroup, InputGroup, FormControl, Button } from 'react-bootstrap';
+
+export default function FlightQuerier() {
+    const Amadeus = require('amadeus');
+    const amadeus = new Amadeus({
+        clientId: 'nrKMtUf0GuX3h7iawhqS4lOSgImGNnCM',
+        clientSecret: 'rWueWloyP8J5JDSt',
+    });
+
+    const flightResults: any[] = []
+    const [resultsState, setResultsState] = useState(flightResults)
+
+    // find all destinations served by a given airport
+    function getDestinations(airportCode: string) {
+        amadeus.shopping.flightDestinations.get({
+            origin: airportCode,
+        }).then((response: any) => {
+            console.log(response.data);
+            const data = response.data;
+            const results: any[] = []
+            for (const result of data) {
+                const flight = {
+                    origin: result.origin,
+                    destination: result.destination,
+                    price: result.price.total,
+                    departureDate: result.departureDate,
+                    returnDate: result.returnDate,
+                }
+                results.push(flight)
+            }
+            setResultsState(results)
+            console.log(flightResults);
+        }).catch((response: any) => {
+            console.log(response);
+        });
+    }
+
+    // return a searchbar which allows the user to search for destinations based on the input
+    // return a list of destinations based on the input
+    return (
+        <div>
+            <InputGroup className="mb-3">
+                <FormControl
+                    placeholder="Departure Airport Code"
+                    aria-label="Departure Airport Code"
+                    aria-describedby="basic-addon2"
+                />
+                <Button variant="outline-secondary" id="button-addon2" onClick={() => {getDestinations('SIN')}}>
+                    Search
+                </Button>
+            </InputGroup>
+            <ListGroup as="div">
+                {resultsState.map((result: any) => {
+                    return (
+                        <ListGroup.Item as="button">
+                            From {result.origin} on {result.departureDate}<br />
+                            To {result.destination} on {result.returnDate}<br />
+                            Price: {result.price}
+                        </ListGroup.Item>
+                    )
+                })}
+            </ListGroup>
+        </div>
+    )
+
+}
