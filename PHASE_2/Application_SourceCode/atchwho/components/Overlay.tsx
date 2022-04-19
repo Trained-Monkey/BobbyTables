0911822bd6f1e81ce75b90c79a691c7897d4cf57
Jamie Domingo <jamiedomingo@JCD-Macbook.local>
2022-04-19 17:59:37 +1000

    BT-70: Overlay text

--- PHASE_2/Application_SourceCode/atchwho/components/Overlay.tsx
+++ PHASE_2/Application_SourceCode/atchwho/components/Overlay.tsx
@@ -1,13 +1,23 @@
 import React, {useState} from "react";
 import { Button } from 'react-bootstrap';
+import Carousel from 'react-bootstrap/Carousel'
 import styled from "styled-components";
-import Modal from 'react-bootstrap/Modal'
+import Modal from 'react-bootstrap/Modal';
+
+import AusImage from './OverlayAssets/Aus.png';
 
 const Overlay = () => {
     const [show, setShow] = useState(true);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+
+    const what = ("AchWHO is your favourite source for accurate, up-to-date information about ongoing pandemics and outbreaks around the world. Data from the World Health Organisation combined with an interactive map simplifies the process of examining specific countries and planning appropriate safety measures.")
+    /* const how = [
+        "EXPLORE:",
+        "ANALYSE:",
+        "MONITOR:"
+    ] */
 
     return(
         <>
@@ -29,9 +39,19 @@ const Overlay = () => {
 
                 <Modal.Body>
                     <h5>What is achWHO?</h5>
-                    <p>hdfhidhf</p>
+                    <p>{what}</p>
                     <h5>How to use achWHO:</h5>
-                    <p>hdfhidhf</p>
+                    <ul>
+                        <li>
+                            EXPLORE: Wander around the map and select your countries of interest. 
+                        </li>
+                        <li>
+                            ANALYSE: Use filters to locate the information you need and observe important reports regarding areas of concern. 
+                        </li>
+                        <li>
+                            MONITOR: Subscribing to push notification updates to continue to stay up-to-date and thus effectively plan safe travel routes. 
+                        </li>
+                    </ul>
                 </Modal.Body>
 
             </LandingModal>
